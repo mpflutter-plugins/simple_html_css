a4d2c3e80261914a259cac85675b7169f6c3b528
PonyCui <cuis@vip.qq.com>
2021-12-23 22:15:06 +0800

    feat: Compat mpflutter.

--- lib/src/css_named_colors.dart
+++ lib/src/css_named_colors.dart
@@ -27,7 +27,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import 'dart:ui' show Color;
+import 'package:flutter/ui/ui.dart' show Color;
 
 class CssNamedColors {
   /// Returns the corresponding [Color] for the passed in CSS color [name]
